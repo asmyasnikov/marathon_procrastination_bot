563a0ce5fd3ed16d86d4f28ff6080281104bdaf0
Aleksey Myasnikov <asmyasnikov@yandex-team.com>
2024-01-06 22:33:01 +0300

    refactoring magic number

--- internal/env/env.go
+++ internal/env/env.go
@@ -8,14 +8,16 @@ import (
 const (
 	YDB_CONNECTION_STRING = "YDB_CONNECTION_STRING"
 	TELEGRAM_TOKEN        = "TELEGRAM_TOKEN"
-	MAGIC_NUMBER          = 347863284
+	MAGIC_NUMBER          = "MAGIC_NUMBER"
+
+	magicNumber = 347863284
 )
 
 func Magic() int {
-	if v, has := os.LookupEnv("MAGIC_NUMBER"); !has {
-		return MAGIC_NUMBER
+	if v, has := os.LookupEnv(MAGIC_NUMBER); !has {
+		return magicNumber
 	} else if vv, err := strconv.Atoi(v); err != nil {
-		return MAGIC_NUMBER
+		return magicNumber
 	} else {
 		return vv
 	}
