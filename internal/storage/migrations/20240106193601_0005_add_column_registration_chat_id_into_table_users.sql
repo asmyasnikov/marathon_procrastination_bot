08a5e9be3875f1fb3b21bf71d5ccb6e418afaeb6
Aleksey Myasnikov <asmyasnikov@yandex-team.com>
2024-01-06 19:37:19 +0300

    added column registration_chat_id into table users

---
+++ internal/storage/migrations/20240106193601_0005_add_column_registration_chat_id_into_table_users.sql
@@ -0,0 +1,9 @@
+-- +goose Up
+-- +goose StatementBegin
+ALTER TABLE users ADD COLUMN registration_chat_id Int64;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+ALTER TABLE users DROP COLUMN registration_chat_id ;
+-- +goose StatementEnd
